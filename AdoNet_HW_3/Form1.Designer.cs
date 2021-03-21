b7c99b5b718326df2d4cb70a327185cc46d2916d
Somewhere <66011798+Somwere@users.noreply.github.com>
2021-03-22 00:42:55 +0200

    Init

---
+++ AdoNet_HW_3/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace AdoNet_HW_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.btnModifiedValues = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(38, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(651, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(38, 201);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(651, 165);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // btnModifiedValues
+            // 
+            this.btnModifiedValues.Location = new System.Drawing.Point(313, 388);
+            this.btnModifiedValues.Name = "btnModifiedValues";
+            this.btnModifiedValues.Size = new System.Drawing.Size(123, 50);
+            this.btnModifiedValues.TabIndex = 2;
+            this.btnModifiedValues.Text = "Modify";
+            this.btnModifiedValues.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(724, 469);
+            this.Controls.Add(this.btnModifiedValues);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "DataReader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button btnModifiedValues;
+    }
+}
+
---
+++ AdoNet_HW_3/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoNet_HW_3
+{
+    public partial class Form1 : Form
+    {
+        string connection = @"Data Source = SQL5103.site4now.net; Initial Catalog = DB_A7168B_kuvaevsql; User Id = DB_A7168B_kuvaevsql_admin; Password=qwerty009";
+        SqlConnection sqlConnection = null;
+        SqlDataAdapter sqlDateAdapter = null;
+        DataSet dataSet = null;
+        SqlCommandBuilder sqlCommanBulder = null;
+        public Form1()
+        {
+            InitializeComponent();
+            sqlConnection = new SqlConnection(connection);
+            btnModifiedValues.Click += BtnModifiedValues_Click; ;
+            try
+            {
+                string query = "select * from Customers";
+                dataGridView1.DataSource = null;
+                dataSet = new DataSet();
+                sqlDateAdapter = new SqlDataAdapter(query, sqlConnection);
+                sqlCommanBulder = new SqlCommandBuilder(sqlDateAdapter);
+                sqlDateAdapter.Fill(dataSet);
+                dataGridView1.DataSource = dataSet.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (sqlConnection != null)
+                    sqlConnection.Close();
+            }
+        }
+
+        private void BtnModifiedValues_Click(object sender, EventArgs e)
+        {
+            dataGridView2.DataSource = null;
+            dataGridView2.DataSource = dataSet.Tables[0].GetChanges(DataRowState.Modified);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
